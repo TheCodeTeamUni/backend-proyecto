f0059ccef756979f454d08549fafc06e3989a426
AndresAlba <o.alba@uniandes.edu.co>
2023-10-05 16:07:58 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Creating a virtual environment in Windows
+
+1. Open a command prompt and navigate to the directory where you want to create the virtual environment.
+2. Type `python -m venv .venv` and press Enter.
+3. To activate the virtual environment, type `.venv\Scripts\activate` and press Enter.
+4. You can now install packages and run your Python code within the virtual environment.
+5. To install requirements, type `python -m pip install -r requirements.txt`
+6. To deactivate the virtual enviroment, type `deactivate`
---
+++ users/application.py
@@ -0,0 +1,22 @@
+from src import create_app
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from src.models import db
+from src.views import ViewUsers, VistaSignUp, VistaPong
+
+application = create_app('default')
+app_context = application.app_context()
+app_context.push()
+
+db.init_app(application)
+db.create_all()
+
+api = Api(application)
+api.add_resource(ViewUsers, '/users/all')
+api.add_resource(VistaSignUp, '/users/signup')
+api.add_resource(VistaPong, '/')
+
+jwt = JWTManager(application)
+
+if __name__ == "__main__":
+    application.run(host = "0.0.0.0", port = 3001, debug = True)
